51664c4fd07ed6edcbf5215009afa43b3b8d7657
Covaciu Andrei <covaciuandrei21@gmail.com>
2024-07-04 11:04:39 +0300

    created a navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
+import logo from "../src/assets/images/logo.svg";
 import "./App.css";
-import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img
-          width="100px"
-          height="100px"
-          src={logo}
-          className="App-logo"
-          alt="logo"
-        />
+        <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
   );
---
+++ src/components/nav.js
@@ -0,0 +1,24 @@
+import "./nav.css";
+
+function Nav() {
+  return (
+    <nav className="nav">
+      <ul>
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#news">News</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
